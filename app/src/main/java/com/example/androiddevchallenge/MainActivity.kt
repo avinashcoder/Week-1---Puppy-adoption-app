c4f51a8ce9e0c8a1981a65e8c447dd5c5c3b0fb6
Avinash <avinash@xcelcorp.com>
2021-02-26 19:09:25 +0530

    Added list of puppy

--- app/src/main/java/com/example/androiddevchallenge/MainActivity.kt
+++ app/src/main/java/com/example/androiddevchallenge/MainActivity.kt
@@ -24,6 +24,7 @@ import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
@@ -41,16 +42,16 @@ import com.example.androiddevchallenge.ui.Puppy
 import com.example.androiddevchallenge.ui.theme.MyTheme
 
 class MainActivity : AppCompatActivity() {
-    val puppys = mutableListOf<Puppy>()
+    private val puppyList = mutableListOf<Puppy>()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        addPuppy();
+        addPuppy()
 
         setContent {
             MyTheme {
-                Surface(color = Color.White) {
-                    puppyList(puppys)
+                Surface(color = MaterialTheme.colors.background) {
+                    PuppyList(puppyList)
                 }
 
             }
@@ -58,14 +59,70 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun addPuppy() {
-        puppys.add(Puppy(R.drawable.puppy_white,"Ruby","Dog","Very cute and white puppy with silky hair"))
-        puppys.add(Puppy(R.drawable.puppy_two,"Sandy","Dog","One day, this Siberian Husky could pull a sled across Alaska"))
-        puppys.add(Puppy(R.drawable.puppy_three,"Tommy","Dog","A devoted companion and incredible working dog, a Golden Retriever is loved by all"))
-        puppys.add(Puppy(R.drawable.puppy_two,"Dora","Dog","Poodles, like this pup, can be a variety of solid colors, including blues, grays, silvers, browns, cafe-au-laits, apricots and creams"))
-        puppys.add(Puppy(R.drawable.puppy_white,"Lovely","Dog","One of the cutest things about the Wirehaired Pointing Griffon? His abundant mustache and eyebrows, for sure"))
-        puppys.add(Puppy(R.drawable.puppy_three,"Bajirao","Dog","With a face like this and the personality to match, it's no wonder the Lab has been the most popular breed for 26 years"))
-        puppys.add(Puppy(R.drawable.puppy_two,"Alex","Dog","If you ever wondered what was cuter than one Pug pup, you have your answer. (It's two Pug pups!)"))
-        puppys.add(Puppy(R.drawable.puppy_three,"Mastani","Dog","These unbelievably adorable bat-like ears are characteristic of the French Bulldog"))
+        puppyList.add(
+            Puppy(
+                R.drawable.puppy_white,
+                "Ruby",
+                "Dog",
+                "Very cute and white puppy with silky hair"
+            )
+        )
+        puppyList.add(
+            Puppy(
+                R.drawable.puppy_two,
+                "Sandy",
+                "Dog",
+                "One day, this Siberian Husky could pull a sled across Alaska"
+            )
+        )
+        puppyList.add(
+            Puppy(
+                R.drawable.puppy_three,
+                "Tommy",
+                "Dog",
+                "A devoted companion and incredible working dog, a Golden Retriever is loved by all"
+            )
+        )
+        puppyList.add(
+            Puppy(
+                R.drawable.puppy_two,
+                "Dora",
+                "Dog",
+                "Poodles, like this pup, can be a variety of solid colors, including blues, grays, silvers, browns, apricots and creams"
+            )
+        )
+        puppyList.add(
+            Puppy(
+                R.drawable.puppy_white,
+                "Lovely",
+                "Dog",
+                "One of the cutest things about the Wire haired Pointing Griffon? His abundant mustache and eyebrows, for sure"
+            )
+        )
+        puppyList.add(
+            Puppy(
+                R.drawable.puppy_three,
+                "Charlie",
+                "Dog",
+                "With a face like this and the personality to match, it's no wonder the Lab has been the most popular breed for 26 years"
+            )
+        )
+        puppyList.add(
+            Puppy(
+                R.drawable.puppy_two,
+                "Alex",
+                "Dog",
+                "If you ever wondered what was cuter than one Pug pup, you have your answer. (It's two Pug pups!)"
+            )
+        )
+        puppyList.add(
+            Puppy(
+                R.drawable.puppy_three,
+                "Bailey",
+                "Dog",
+                "These unbelievably adorable bat-like ears are characteristic of the French Bulldog"
+            )
+        )
 
     }
 }
@@ -73,22 +130,31 @@ class MainActivity : AppCompatActivity() {
 // Start building your app here!
 
 @Composable
-fun puppyList(puppys: List<Puppy>) {
+fun PuppyList(puppyList: List<Puppy>, modifier: Modifier = Modifier) {
 
-        LazyColumn (contentPadding = PaddingValues(horizontal = 16.dp, vertical = 8.dp), modifier = Modifier.fillMaxWidth()) {
+    Column() {
+        LazyColumn(
+            modifier
+                .fillMaxSize()
+                .padding(10.dp, 0.dp)
+        ) {
 
-            items(puppys){ puppy ->
-                PuppyRow(puppy)
+            items(puppyList) { ItemPuppy ->
+                PuppyRow(ItemPuppy)
             }
         }
-
+    }
 }
 
 @Composable
 fun PuppyRow(puppy: Puppy) {
-    Column() {
+    Column {
         Spacer(Modifier.height(6.dp))
-        Row(verticalAlignment = Alignment.CenterVertically,modifier = Modifier.padding(PaddingValues(0.dp,6.dp))) {
+
+        Row(
+            verticalAlignment = Alignment.CenterVertically, modifier = Modifier
+                .padding(PaddingValues(0.dp, 6.dp))
+        ) {
             Image(
                 painter = painterResource(puppy.image),
                 contentDescription = null,
@@ -98,18 +164,31 @@ fun PuppyRow(puppy: Puppy) {
                     .clip(shape = RoundedCornerShape(30.dp)),
                 contentScale = ContentScale.Crop
             )
-            Column( modifier = Modifier
-                .padding(PaddingValues(15.dp, 0.dp, 10.dp, 0.dp))
-                .fillMaxWidth()) {
-                Text(puppy.name,fontSize = 18.sp, color = Color.Black)
+            Column(
+                modifier = Modifier
+                    .padding(PaddingValues(15.dp, 0.dp, 10.dp, 0.dp))
+                    .fillMaxWidth()
+            ) {
+                Text(puppy.name, fontSize = 18.sp, color = Color.Black)
                 Text(puppy.type, fontSize = 13.sp, color = Color.DarkGray)
-                Text(puppy.desc, fontSize = 12.sp, color = Color.Gray, maxLines = 1, overflow = TextOverflow.Ellipsis)
+                Text(
+                    puppy.desc,
+                    fontSize = 12.sp,
+                    color = Color.Gray,
+                    maxLines = 1,
+                    overflow = TextOverflow.Ellipsis
+                )
             }
         }
 
         Spacer(Modifier.height(4.dp))
 
-        Box(Modifier.fillMaxWidth().height(1.dp).background(Color.LightGray))
+        Box(
+            Modifier
+                .fillMaxWidth()
+                .height(1.dp)
+                .background(Color.LightGray)
+        )
     }
 
 }
@@ -118,23 +197,23 @@ fun PuppyRow(puppy: Puppy) {
 @Preview("Light Theme", widthDp = 360, heightDp = 640)
 @Composable
 fun LightPreview() {
-    val puppys = mutableListOf<Puppy>()
-    for(i in 0 until 10){
-        puppys.add(Puppy(R.drawable.puppy_white,"Tommy","Dog", "Good barking dog"))
+    val puppyList = mutableListOf<Puppy>()
+    for (i in 0 until 10) {
+        puppyList.add(Puppy(R.drawable.puppy_white, "Tommy", "Dog", "Good barking dog"))
     }
     MyTheme {
-        puppyList(puppys)
+        PuppyList(puppyList)
     }
 }
 
 @Preview("Dark Theme", widthDp = 360, heightDp = 640)
 @Composable
 fun DarkPreview() {
-    val puppys = mutableListOf<Puppy>()
-    for(i in 0 until 10){
-        puppys.add(Puppy(R.drawable.puppy_white,"Tommy","Dog", "Good barking dog"))
+    val puppyList = mutableListOf<Puppy>()
+    for (i in 0 until 10) {
+        puppyList.add(Puppy(R.drawable.puppy_white, "Tommy", "Dog", "Good barking dog"))
     }
     MyTheme(darkTheme = true) {
-        puppyList(puppys)
+        PuppyList(puppyList)
     }
 }
